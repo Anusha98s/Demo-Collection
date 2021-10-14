6d9241e6b9584042212466e3f31513d0b02ad8ee
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 12:04:45 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Demo_Collection
+{
+    class Class1
+    {
+    }
+    var MYLIST = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    
+    foreach (var item in MYLIST)
+
+    
+}
+
+
+    
+    
+
